7d8b17f900f2e65bb8fd5eed1894ae5f583dff2d
yeon119 <128660797+yeon119@users.noreply.github.com>
2026-04-14 15:41:56 +0900

    Add files via upload

---
+++ 202344070_DataBase/database0414.sql
@@ -0,0 +1,64 @@
+use class_c;
+
+
+    SELECT JSON_ARRAYAGG(
+           JSON_OBJECT(
+               'department', department,
+               'student_number', student_number,
+               'name', name,
+               'grade', grade,
+               'cell_phone', cell_phone,
+               'email', email
+           )
+       )
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/dbstudent.json'
+FROM dbstudent;
+
+
+create table info_employees( 
+	emp_num char(9) primary key,
+    emp_name varchar(14)
+    );
+    
+create table info_employee2(
+	emp_num char(9),
+    emp_name varchar(14),
+    constraint pk_info_employee2 primary key(emp_num)
+    );
+
+
+create table const_students(
+	s_jumin_number char(14),
+    s_name varchar(14),
+    constraint uni_s_jumin_number unique(s_jumin_number)
+);
+
+insert into const_students values('11111111111111', '김창수');
+select * from const_students;
+
+insert into const_students values('11111111111111','이한우'); -- unique 떄문에 중복되는 값을 넣을수없음
+
+
+insert into const_students values (null ,'김종혁');
+
+select * from const_students
+where s_jumin_number is null;
+
+insert into const_students values (null, '최여진');
+
+
+-- primary key
+-- 각 행을 고유하게 구별해주는 식별자로 어던행도 똑같은 값을 가질수 없음
+-- unique 와 달리 null 값 입력 불가
+
+
+create table primary_students(
+	s_jumin_number char(14),
+    s_name varchar(14),
+		constraint pk_s_jumin_number primary key(s_jumin_number)
+);
+
+insert into primary_students values('890413-1712343','김재현');
+select * from primary_students;
+
+insert into primary_students values('890413-1712343','김다영'); -- 얘도 중복값이기때문에 에러가남
